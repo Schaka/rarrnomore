a8f291e8ea9108faff1553db9587176b61b865d4
Schaka <2223171+Schaka@users.noreply.github.com>
2024-04-08 20:26:25 +0200

    [Build] Update logging dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+    implementation("org.slf4j:jcl-over-slf4j")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(module = "mockito-core")
     }
